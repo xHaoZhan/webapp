cf0f0e11b31a2ad778b62ed4171ff4328ae11f54
xHaoZhan <ganhaozhan@live.com>
2020-06-11 14:32:16 +0800

    Create view.php

---
+++ view.php
@@ -0,0 +1,11 @@
+<php
+try {
+    $conn = new PDO("sqlsrv:server = tcp:ddacdatabase.database.windows.net,1433; Database = ddacdatabase", "ddacadmin", "ddac@123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+?>
